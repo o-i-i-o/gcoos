7be2fdacc33f8335cbf80fe825a85ff0ca5b4b15
惊蛰 <100140586+o-i-i-o@users.noreply.github.com>
2024-11-22 15:35:41 +0800

    Update 生成证书.cmd

--- 生成证书.cmd
+++ 生成证书.cmd
@@ -10,7 +10,7 @@ echo.  https://github.com/o-i-i-o/gcoos
 echo. ------------------Ver: 0.3a4 ------------------------------------------------------------------
 echo.
 echo. ---------------使用此脚本前请用文本格式打开此脚本并核对openssl环境是否配置正确-------------------
-echo. ---------------环境变量包括配置文件路径cnf\openssl.cnf，key、csr、crt文件生成路径baseCA\certs----------------------
+echo. ---------------环境变量包括配置文件路径cnf\openssl.cnf，key、csr、crt文件生成路径cert-ca\certs----------------------
 echo.
 echo.
 
@@ -21,7 +21,7 @@ IF /i "!ca!"=="n" goto mkca1
 exit
 
 :mkca1
-echo. 通常CA（根证书）只用生成一次，是否继续生成，若baseCA文件夹下已存在ca文件，将报错退出
+echo. 通常CA（根证书）只用生成一次，是否继续生成，若cert-ca文件夹下已存在ca文件，将报错退出
 
 set /p mkcaa=输入y继续生成CA，否则退出:
 IF /i "!mkcaa!"=="y" Goto mkca2
@@ -29,10 +29,10 @@ exit
 
 :mkca2
 set casubj=/C=CN/ST=GuiZhou/L=GuiYang/O=Organization/OU=IT/emailAddress=boss@oi-io.cc
-openssl req -new -x509 -days 3650 -keyout baseCA\ca.key -out baseCA\ca.crt -subj %casubj% -extensions v3_ca
+openssl req -new -x509 -days 3650 -keyout cert-ca\ca.key -out cert-ca\ca.crt -subj %casubj% -extensions v3_ca
 IF %ERRORLEVEL% NEQ 0 ( echo. 生成失败，请检查配置，即将退出!
 
-exit ) else ( echo. 生成成功，CA保存在baseCA\baseCA\ca.key、baseCA\ca.crt；请确定ca信息是否正确
+exit ) else ( echo. 生成成功，CA保存在cert-ca\cert-ca\ca.key、cert-ca\ca.crt；请确定ca信息是否正确
 
 echo. 若需修改扩展信息，请直接修改脚本对应字段 )
 
@@ -83,19 +83,19 @@ Pause>Nul&Goto :keytype
 :ECCKEY
 echo. 即将生成ECC私钥
 
-openssl ecparam -genkey -name secp384r1 -out baseCA\certs\%certname%.key
+openssl ecparam -genkey -name secp384r1 -out cert-ca\certs\%certname%.key
 echo. 若需修改ECC私钥强度，请直接修改脚本的secp384r1字段.
 
-echo. ECC私钥保存在baseCA\certs\%certname%.key
+echo. ECC私钥保存在cert-ca\certs\%certname%.key
 pause
 goto goon
 
 
 :RSAKEY
 echo. 即将生成RSA私钥
-openssl genrsa -out baseCA\certs\%certname%.key 2048
+openssl genrsa -out cert-ca\certs\%certname%.key 2048
 echo. 若需修改RSA私钥强度，请直接修改脚本的2048字段.
-echo. RSA私钥保存在baseCA\certs\%certname%.key
+echo. RSA私钥保存在cert-ca\certs\%certname%.key
 pause
 goto goon
 
@@ -103,9 +103,9 @@ goto goon
 echo.
 echo. 2、生成请求文件
 set crtsubj=/C=CN/ST=GuiZhou/L=GuiYang/O=Organization/OU=IT/CN=%certname%/emailAddress=boss@oi-io.cc
-openssl req -new   -key baseCA\certs\%certname%.key -out baseCA\certs\%certname%.csr -subj %crtsubj%
+openssl req -new   -key cert-ca\certs\%certname%.key -out cert-ca\certs\%certname%.csr -subj %crtsubj%
 echo. 若需修改扩展信息，请直接修改脚本-subj后对应字段.
-echo. 请求文件保存在baseCA\certs\%certname%.csr
+echo. 请求文件保存在cert-ca\certs\%certname%.csr
 pause
 
 echo.
@@ -124,7 +124,7 @@ echo.
 echo. 4、生成用户证书.
 
 echo. 需求:输错密码可以选择重新输入而不是直接退出(密码错有error password字段）
-openssl ca -in baseCA\certs\%certname%.csr -out baseCA\certs\%certname%.crt -cert baseCA\ca.crt -keyfile baseCA\ca.key -extensions v3_req -config cnf\%certname%.cnf -days 3650 
+openssl ca -in cert-ca\certs\%certname%.csr -out cert-ca\certs\%certname%.crt -cert cert-ca\ca.crt -keyfile cert-ca\ca.key -extensions v3_req -config cnf\%certname%.cnf -days 3650 
 IF %ERRORLEVEL% NEQ 0 ( echo. 删除缓存配置文件
 del cnf\temp 
 GOTO error ) else (echo. 删除缓存配置文件
@@ -133,7 +133,7 @@ GOTO OK)
 
 :OK
 echo.
-echo. 生成成功，用户证书保存在baseCA\certs\%certname%.crt
+echo. 生成成功，用户证书保存在cert-ca\certs\%certname%.crt
 echo. 证书有效期3650天，若需修改有效期，请直接修改脚本对应字段.
 pause
 exit
@@ -142,8 +142,8 @@ exit
 echo.
 ECHO. 错误
 echo. 删除生成的文件
-del baseCA\certs\%certname%.key
-del baseCA\certs\%certname%.csr
+del cert-ca\certs\%certname%.key
+del cert-ca\certs\%certname%.csr
 del cnf\%certname%.cnf
 Echo. 请检查ca密码或各项配置
 pause
