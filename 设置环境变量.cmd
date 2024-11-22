ec8d67ed010de54eb9545f0208bd986c84dd281b
惊蛰 <100140586+o-i-i-o@users.noreply.github.com>
2024-11-22 15:42:51 +0800

    Update 设置环境变量.cmd

--- 设置环境变量.cmd
+++ 设置环境变量.cmd
@@ -92,7 +92,7 @@ echo. 创建%path%\cert-ca\pemcerts成功。
 type nul >> cert-ca\index.txt
 echo. 创建%path%\cert-ca\index.txt成功。
 
-echo. 00 >> cert-ca\serial
+echo.00 >> cert-ca\serial
 echo. 创建%path%\cert-ca\serial成功。
 
 echo. 复制配置文件到cnf文件夹。
